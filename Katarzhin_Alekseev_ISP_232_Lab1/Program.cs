b286e664dcf14c2c40852a3798ea57736e6784e6
antres <lrweor2>
2025-09-06 10:18:11 +0300

    Add project files.

---
+++ Katarzhin_Alekseev_ISP_232_Lab1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Katarzhin_Alekseev_ISP_232_Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
